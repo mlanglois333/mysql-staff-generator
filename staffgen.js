183732cbdd817df869d1814c14e0cbb728a0d29d
Marc <mlanglois333@icloud.com>
2020-09-06 11:26:47 -0400

    Test init

--- staffgen.js
+++ staffgen.js
@@ -2,17 +2,17 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table")
 var connection = mysql.createConnection({
+    
     host: "localhost",
-
-
     port: 3306,
     user: "root",
     password: "",
     database: "staffgen"
 });
 
-connection.connect(function (err) {
+connection.connect(function (err, results) {
     if (err) throw err;
+    console.table(results);
     init();
 });
 
