537a87ac8374608461e51c35487342d263234df4
Marc <mlanglois333@icloud.com>
2020-09-06 08:05:11 -0400

    dependencies

--- staffgen.js
+++ staffgen.js
@@ -1,6 +1,6 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table")
 var connection = mysql.createConnection({
   host: "localhost",
 
