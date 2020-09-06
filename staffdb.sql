70ec9f86a84f86b7b96a4d93449774de24b2046f
Marc <mlanglois333@icloud.com>
2020-09-06 10:37:40 -0400

    added content/keys

--- staffdb.sql
+++ staffdb.sql
@@ -23,7 +23,13 @@ CREATE TABLE IF NOT EXISTS `department` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table staffgen.department: ~0 rows (approximately)
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` (`id`, `name`) VALUES
+	(123, 'HR'),
+	(234, 'Development'),
+	(345, 'Facilities');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
 
 -- Dumping structure for table staffgen.employee
 CREATE TABLE IF NOT EXISTS `employee` (
@@ -32,10 +38,26 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `last_name` varchar(50) DEFAULT NULL,
   `role_id` int DEFAULT NULL,
   `manager_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `role_fk` (`role_id`),
+  CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table staffgen.employee: ~0 rows (approximately)
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `role_id`, `manager_id`) VALUES
+	(1, 'Peter', 'Janakowski', 6, 2),
+	(2, 'Rob', 'Halford', 7, NULL),
+	(3, 'Steve', 'Soto', 1, 444),
+	(4, 'Ronnie  James', 'Dio', 2, 444),
+	(5, 'Carl', 'Branananalowski', 1, 333),
+	(6, 'Neil', 'Young', 2, 333),
+	(7, 'Rick', 'Agnew', 4, 555),
+	(8, 'Ian', 'Mackeye', 5, 555),
+	(333, 'Scott', 'Hull', 3, NULL),
+	(444, 'Kerry', 'King', 3, NULL),
+	(555, 'Jello', 'Biafra', 8, NULL);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 
 -- Dumping structure for table staffgen.role
 CREATE TABLE IF NOT EXISTS `role` (
@@ -43,10 +65,23 @@ CREATE TABLE IF NOT EXISTS `role` (
   `title` varchar(30) NOT NULL DEFAULT '',
   `salary` decimal(10,0) NOT NULL DEFAULT '0',
   `department_id` int NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `department_key` (`department_id`),
+  CONSTRAINT `department_key` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table staffgen.role: ~0 rows (approximately)
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` (`id`, `title`, `salary`, `department_id`) VALUES
+	(1, 'Engineer', 50000, 234),
+	(2, 'Senior Engineer', 100000, 234),
+	(3, 'Manager', 100001, 234),
+	(4, 'Entry HR', 60000, 123),
+	(5, 'Senior HR', 90000, 123),
+	(6, 'Custodian', 50000, 345),
+	(7, 'Facilities Manager', 91000, 345),
+	(8, 'Manager HR', 100000, 123);
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
