7241d5601f306c311284cca5894199b9cc969692
Marc <mlanglois333@icloud.com>
2020-09-07 09:48:11 -0400

    Debugging add function

--- staffgen.js
+++ staffgen.js
@@ -2,10 +2,11 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table")
 let departments = ["Human Resources", "Development", "Facilities"];
-let managers = ["2", "333", "444", "555"]
-let roles = ["Engineer", "Senior Engineer", "Manager", "Entry HR", "Senior HR", "Custodian", "Facilities Manager", "Manager HR"]
+let managers = [2, 333, 444, 555];
+let roles = ["Engineer", "Senior Engineer", "Manager", "Entry HR", "Senior HR", "Custodian", "Facilities Manager", "Manager HR"];
+let roleIds =[1, 2, 3, 4, 5, 6, 7, 8];
 const connection = mysql.createConnection({
-    
+
     host: "localhost",
     port: 3306,
     user: "root",
@@ -20,20 +21,20 @@ connection.connect(function (err) {
 
 function displayQuery(qu) {
 
-    connection.query(qu, function(err, res){
+    connection.query(qu, function (err, res) {
         if (err) throw err;
         console.table(res);
     })
 }
 
 function init() {
-var query = "SELECT employee.first_name, employee.last_name, role.title, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id" ;
-console.log("Welcome to the staff database! Here are our current employees:")
-connection.query(query, function(err,res){
-    if (err) throw err;
-    console.table(res);
-    menu();
-});
+    var query = "SELECT employee.first_name, employee.last_name, role.title, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id";
+    console.log("Welcome to the staff database! Here are our current employees:")
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        menu();
+    });
 }
 
 function menu() {
@@ -46,6 +47,7 @@ function menu() {
             "View employees by department",
             "View employees by manager",
             "View all roles",
+            "Add new employee",
             "Remove Employee",
             "Update Employee Role",
             "Update Employee Manager"
@@ -62,11 +64,15 @@ function menu() {
                 break;
 
             case "View employees by manager":
-                  viewByMan();
+                viewByMan();
                 break;
 
             case "View all roles":
                 viewRoles();
+                break;
+
+            case "Add new employee":
+                addEmp()
                 break;
 
             case "Remove Employee":
@@ -87,9 +93,9 @@ function menu() {
 }
 
 
-function viewAll() { 
-    var query = "SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, employee.role_id, role.title, role.salary, department.id, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id" ;
-    connection.query(query, function(err,res){
+function viewAll() {
+    var query = "SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, employee.role_id, role.title, role.salary, department.id, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id";
+    connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
         menu();
@@ -97,99 +103,122 @@ function viewAll() {
 }
 
 function viewByDept() {
-    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id WHERE ?" ;
+    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id WHERE ?";
     inquirer.prompt({
         name: "dept",
         type: "list",
         message: "Which department would you like to view?",
         choices: departments
-        }).then(function (answer) {
-            connection.query(query, {name: answer.dept}, function(err, res){
-                if (err) throw err;
-               console.table(res);
-               menu();
-            })
-            
+    }).then(function (answer) {
+        connection.query(query, { name: answer.dept }, function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            menu();
+        })
+
     })
 }
 
 function viewByMan() {
-    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id WHERE ?" ;
+    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id WHERE ?";
     inquirer.prompt({
         name: "man",
         type: "list",
         message: "Select manager ID number",
         choices: managers
-        }).then(function (answer) {
-            connection.query(query, {manager_id: answer.man}, function(err, res){
-                if (err) throw err;
-               console.table(res); 
-               menu();
-            })
-            
+    }).then(function (answer) {
+        connection.query(query, { manager_id: answer.man }, function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            menu();
+        })
+
     })
- }
+}
 
 function viewRoles() {
 
-    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id WHERE ?" ;
+    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id WHERE ?";
     inquirer.prompt({
         name: "role",
         type: "list",
         message: "Which roles would you like to view?",
         choices: roles
-        }).then(function (answer) {
-            connection.query(query, {name: answer.roles}, function(err, res){
-                if (err) throw err;
-               console.table(res); 
-            })
-            
+    }).then(function (answer) {
+        connection.query(query, { title: answer.role }, function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            menu();
+        })
+
     })
- }
+}
 
 function addEmp() {
 
     inquirer
-    .prompt({
-      name: "empid",
-      type: "input",
-      message: "ID number of new employee:"
-    },
-    {
-        name: "empfirst",
-        type: "input",
-        message: "First name:" 
-    },
-    {
-        name: "emplast",
-        type: "input",
-        message: "Last name" 
-    },
-    {
-        name: "emprole",
-        type: "list",
-        message: "Role:",
-        choices: roles ,
+        .prompt([
+            {
+            name: "empid",
+            type: "input",
+            message: "ID number of new employee:"
+        },
+            {
+                name: "empfirst",
+                type: "input",
+                message: "First name:"
+            },
+            {
+                name: "emplast",
+                type: "input",
+                message: "Last name"
+            },
+            {
+                name: "emprole",
+                type: "list",
+                message: "Role ID:",
+                choices: roleIds
 
-    },
-    {
-        name: "empman",
-        type: "list",
-        message: "Manager ID",
-        choices: managers 
-    },).then(function(answer) {
-      var query = `INSERT INTO employee (id,first_name,last_name,role_id,manager_id VALUES (${answer.empid},${answer.empfirst},${answer.emplast},${answer.emprole},${answer.empman}`;
-      connection.query(query, function(err,res){
-        if (err) throw err;
-        console.log(`${answer.empfirst} ${answer.emplast} has been added to the database`);
-        menu();
-    });
+            },
+            {
+                name: "empman",
+                type: "list",
+                message: "Manager ID",
+                choices: managers
+            
+            }]).then(function (answer) {
+                var numid= Number(answer.empid);
+                console.log(numid);
+                console.log(typeof answer.empid);
+                var query = 'INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (' + numid +',' + answer.empfirst +',' + answer.emplast +',' + answer.emprole +',' + answer.empman +')';
+                connection.query(query, function (err, res) {
+                    if (err) throw err;
+                    console.log(`${answer.empfirst} ${answer.emplast} has been added to the database`);
+                    menu();
+                });
 
-})
+            })
 }
 
 
-function removeEmp() { }
+function removeEmp() {
+
+    inquirer
+        .prompt({
+            name: "remove",
+            type: "input",
+            message: "ID number of employee to remove:"
+        })
+        .then(function (answer) {
+            var query = `DELETE FROM employee WHERE id = ${answer.remove}`;
+            connection.query(query, function (err, res) {
+                if (err) throw err;
+                console.log(`Employee number ${answer.input} has been removed.`)
+
+
+            });
+        });
+    }
 
 function updateEmpRole() { }
 
